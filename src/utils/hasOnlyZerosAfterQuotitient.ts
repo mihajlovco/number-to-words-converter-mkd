985f083ef72ca76c4311a43928eab92b883e2e6a
Sasho Mihajlov <sasho@mihajlov.co>
2026-04-30 10:01:47 +0200

    fix: typo function name

--- src/utils/hasOnlyZerosAfterQuotitient.ts
+++
@@ -1,6 +0,0 @@
-import { hasOnlyZerosAfterQuotient } from './hasOnlyZerosAfterQuotient';
-
-export { hasOnlyZerosAfterQuotient } from './hasOnlyZerosAfterQuotient';
-
-// Backward-compatible alias for internal typo migration.
-export const hasOnlyZerosAfterQuotitient = hasOnlyZerosAfterQuotient;
